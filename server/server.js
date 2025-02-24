9d8f7375877c5cf0a9d225919bf7a96371a8ab5a
SimPresko <preslibankov@gmail.com>
2025-02-24 23:05:04 +0100

    Version 2 - Add a bit of styling - Fix canvas - Change key pressing from the client to be able to go diagonally - Fix uneven movement on keydown with fixed speed - Fix diagonal acceleration with sqrt - Fix going in opposite direction

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const socketIo = require('socket.io')
 const path = require('path')
+const { SourceTextModule } = require('vm')
 const app = express()
 const server = http.createServer(app)
 const io = socketIo(server)
@@ -18,23 +19,50 @@ server.listen(2000, () => {
 })
 
 let players = {}
+let keyStates = {}
+
+const SPEED = 10
+const TICK_SPEED = 1000 / 30
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
-  players[socket.id] = { x: 0, y: 0 }
+  console.log('A user connected')
+  players[socket.id] = {
+    x: Math.floor(Math.random() * 30) * 20 + 100,
+    y: Math.floor(Math.random() * 20) * 20 + 100,
+  }
+  console.log(players[socket.id])
 
-  socket.on('move', (data) => {
-    players[socket.id].x += data.dx
-    players[socket.id].y += data.dy
+  socket.on('keyState', (keys) => {
+    keyStates[socket.id] = keys
   })
 
   socket.on('disconnect', () => {
-    console.log('user disconnected')
+    console.log('User disconnected')
     delete players[socket.id]
   })
 })
 
-const framerate = 30
 setInterval(function () {
+  for (let id in players) {
+    if (!keyStates[id]) continue
+
+    let player = players[id]
+    let keys = keyStates[id]
+
+    let moveX =
+      (keys['ArrowRight'] || keys['d'] ? 1 : 0) -
+      (keys['ArrowLeft'] || keys['a'] ? 1 : 0)
+    let moveY =
+      (keys['ArrowDown'] || keys['s'] ? 1 : 0) -
+      (keys['ArrowUp'] || keys['w'] ? 1 : 0)
+
+    if (moveX !== 0 && moveY !== 0) {
+      moveX *= Math.SQRT1_2
+      moveY *= Math.SQRT1_2
+    }
+
+    player.x = Math.max(0, Math.min(880, player.x + moveX * SPEED))
+    player.y = Math.max(0, Math.min(580, player.y + moveY * SPEED))
+  }
   io.emit('state', players)
-}, 1000 / framerate)
+}, TICK_SPEED)
